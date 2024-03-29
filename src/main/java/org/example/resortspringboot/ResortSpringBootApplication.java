f73c487aef71d67760a1063d3e285170fa03834e
Giao U <lesliegiao.u@gmail.com>
2024-03-29 17:00:18 -0500

    initialize the project

---
+++ src/main/java/org/example/resortspringboot/ResortSpringBootApplication.java
@@ -0,0 +1,20 @@
+package org.example.resortspringboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class ResortSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResortSpringBootApplication.class, args);
+    }
+
+
+
+
+
+}
---
+++ src/test/java/org/example/resortspringboot/ResortSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resortspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResortSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
