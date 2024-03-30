ab3bad6401602de995fd95f0b5fc301b74cb6c72
Giao U <lesliegiao.u@gmail.com>
2024-03-29 20:15:16 -0500

    Modified service file to reflect the change in the database

--- src/main/java/org/example/resortspringboot/Customer/CustomerService.java
+++ src/main/java/org/example/resortspringboot/Customer/CustomerService.java
@@ -1,24 +1,19 @@
 package org.example.resortspringboot.Customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 @Service
 public class CustomerService {
-    public List<Customer> getCustomer(){
-        return List.of(
-                new Customer(
-                        1L,
-                        "U",
-                        "Leslie",
-                        "WA123",
-                        "123",
-                        "leslie@gmail.com",
-                        LocalDate.of(1997,07,25),
-                        26
-                )
 
-        );
+    final private CustomerRepository customerRepository;
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+    public List<Customer> getCustomer(){
+        return customerRepository.findAll();
     }
 }
